4474c23c7fbeff01c7f9cc3291b3f810ebd95fa3
Joshua Wright <jwright@hasborg.com>
2019-04-08 07:35:43 -0400

    Add gitignore, remove deploy.sh (local only script)

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-if [ "z"$1 = "zprod" ] ; then
-    rsync -ave ssh --delete --exclude='.git/' . piprintprod:pialtprinter/
-else
-    rsync -ave ssh --delete --exclude='.git/' . piprint:pialtprinter/
-fi
